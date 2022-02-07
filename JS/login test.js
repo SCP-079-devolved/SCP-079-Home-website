0bc31f25cfae94c9126e26f70eed8a600c090ebe
Carnoval 15 <carnoval@protonmail.com>
2022-02-07 15:31:59 +0330

    backend is completed, now we only have to add the content for early supporters (i guess)

--- JS/login test.js
+++ JS/login test.js
@@ -1,56 +1,73 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 var firebaseConfig = {
-    apiKey: "AIzaSyC7_Bp9ms-MeW1gDXciSAHLmIUMusnSsm4",
-    authDomain: "scp-079-devolved.firebaseapp.com",
-    projectId: "scp-079-devolved",
-    storageBucket: "scp-079-devolved.appspot.com",
-    messagingSenderId: "235107881289",
-    appId: "1:235107881289:web:1ac489cad1506b353c4a98",
-    measurementId: "G-09W47NH25M"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+  apiKey: "AIzaSyC7_Bp9ms-MeW1gDXciSAHLmIUMusnSsm4",
+  authDomain: "scp-079-devolved.firebaseapp.com",
+  projectId: "scp-079-devolved",
+  storageBucket: "scp-079-devolved.appspot.com",
+  messagingSenderId: "235107881289",
+  appId: "1:235107881289:web:1ac489cad1506b353c4a98",
+  measurementId: "G-09W47NH25M"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-  const auth =  firebase.auth();
+const auth = firebase.auth();
 
-  //signup function
-  function signUp(){
-    var email = document.getElementById("email");
-    var password = document.getElementById("password");
+function signed_out() {
+  var login = document.getElementById("login");
+  var signedin = document.getElementById("signedin");
 
-    const promise = auth.createUserWithEmailAndPassword(email.value,password.value);
-    //
-    promise.catch(e=>alert(e.message));
-    alert("SignUp Successfully");
+  signedin.style.display = "none";
+  login.style.display = "block";
+}
+
+function signed_in(){
+  var login = document.getElementById("login");
+  var signedin = document.getElementById("signedin");
+
+  signedin.style.display = "block";
+  login.style.display = "none";
+}
+
+
+
+//signup function
+function signUp() {
+  var email = document.getElementById("email");
+  var password = document.getElementById("password");
+
+  const promise = auth.createUserWithEmailAndPassword(email.value, password.value);
+  //
+  promise.catch(e => alert(e.message));
+  alert("SignUp Successfully");
+}
+
+//signIN function
+function signIn() {
+  var email = document.getElementById("email");
+  var password = document.getElementById("password");
+  const promise = auth.signInWithEmailAndPassword(email.value, password.value);
+  promise.catch(e => alert(e.message));
+
+}
+
+
+//signOut
+
+function signOut() {
+  auth.signOut();
+  alert("SignOut Successfully from System");
+}
+
+//active user to homepage
+firebase.auth().onAuthStateChanged((user) => {
+  if (user) {
+    var email = user.email;
+    alert("Active user " + email);
+
+    signed_in();
+  } else {
+    alert("No Active user Found")
+    signed_out();
   }
-
-  //signIN function
-  function  signIn(){
-    var email = document.getElementById("email");
-    var password  = document.getElementById("password");
-    const promise = auth.signInWithEmailAndPassword(email.value,password.value);
-    promise.catch(e=>alert(e.message));
-    
-  }
-
-
-  //signOut
-
-  function signOut(){
-    auth.signOut();
-    alert("SignOut Successfully from System");
-  }
-
-  var i = 0;
-
-  //active user to homepage
-  firebase.auth().onAuthStateChanged((user)=>{
-    if(user){
-      var i = 1;
-      var email = user.email;
-      alert("Active user "+email);
-
-    }else{
-      alert("No Active user Found")
-    }
-  })
+})
